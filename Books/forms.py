1d3fe74faed72a5b75c9006bfb1e1f2f1f65ca19
swapno963 <swapno963@gmail.com>
2024-02-17 02:47:10 +0600

    some style changes

--- Books/forms.py
+++ Books/forms.py
@@ -1,8 +1,16 @@
 from django import forms
-from Books.models import CommentModel
-
+from .models import CommentModel
 
 class CommentForm(forms.ModelForm):
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.fields['body'].widget.attrs.update({
+            'class': 'bg-gray-100 rounded-lg border border-gray-300 py-2 px-4 w-full',
+            'placeholder': 'Write your comment here...',
+             'rows': 7, 
+             'cols': 60, 
+        })
+
     class Meta:
         model = CommentModel
-        fields = ['name','body']
+        fields = ['body']
